17da9ca61473bb40d05d0c9deba7e55d5c36c02d
B1ll7 <malarbillaudrey@gmail.com>
2019-02-12 16:39:34 +0100

    suite des exos réalisés correction de quelques points.. et correction de l'ex5-6 MVC

--- MVC2/Controllers/BrokerController.cs
+++ MVC2/Controllers/BrokerController.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Text.RegularExpressions; //Pour utiliser les regex
+using System.Text.RegularExpressions; // Pour l'utilisation des Regex
 using MVC2.Models;
+using System.Data.Entity;
+using System.Net;
 
 namespace MVC2.Controllers
 {
@@ -12,14 +12,96 @@ namespace MVC2.Controllers
     {
         private NoteBookEntities1 db = new NoteBookEntities1();
 
-
-        // GET: ProfilBroker
-        public ActionResult ProfilBroker()
-        {
-            return View();
+        // GET: Une methode qui permet d'afficher le ProfilBroker
+        public ActionResult ProfilBroker(int? id) // '?' rendre nullable l'id
+        { 
+            if (id == null)
+            {
+                return View("ErrorBadRequest");
+            }
+            // recupère le broker par son id. 
+            broker brokerById = db.brokers.Find(id);
+            if (brokerById == null)
+            {
+                return View("ErrorPageNotFound");
+            }
+            return View(brokerById);
         }
-
-        // GET: ListBroker
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult ProfilBroker([Bind(Include = "idBroker,lastName,firstName,mail,phoneNumber")] broker brokerList)
+        {
+            //Déclaration des Regexs
+            string regexName = @"^[A-Za-zéèàêâôûùïüç-]+$";
+            string regexMail = @"^[0-9a-zA-Z.-]+@[0-9a-zA-Z.-]+.[a-zA-Z]{2,4}";
+            string regexPhone = @"^[0][0-9]{9}";
+            //Vérification que le champ lastName ne soit pas incorrect ou null
+            if (!String.IsNullOrEmpty(brokerList.lastName))  //si le champ mail n'est pas vide ou null on vérifie la validité de l'entrée
+            {
+                if (!Regex.IsMatch(brokerList.lastName, regexName))
+                {
+                    //Message d'erreur
+                    ModelState.AddModelError("lastName", "Votre nom n'est pas valide");
+                }
+            }
+            else
+            {
+                //Message d'erreur si le lastName est vide ou null
+                ModelState.AddModelError("lastName", "Veuillez remplir ce champ");
+            }
+            //Vérification que le champ firstName ne soit pas incorrect ou null
+            if (!String.IsNullOrEmpty(brokerList.firstName)) //si le champ firstName n'est pas vide ou null on vérifie la validité de l'entrée
+            {
+                if (!Regex.IsMatch(brokerList.firstName, regexName)) //si l'entrée utilisateur ne passe pas la regex ajout d'un message d'erreur
+                {
+                    //Message d'erreur
+                    ModelState.AddModelError("firstName", "Votre prénom n'est pas valide");
+                }
+            }
+            else
+            {
+                //Message d'erreur si le firstName est vide ou null
+                ModelState.AddModelError("firstName", "Veuillez remplir ce champ");
+            }
+            //Vérification que le champ email ne soit pas incorrect ou null
+            if (!String.IsNullOrEmpty(brokerList.mail)) //si le champ mail n'est pas vide ou null on vérifie la validité de l'entrée
+            {
+                if (!Regex.IsMatch(brokerList.mail, regexMail))
+                {
+                    //Message d'erreur
+                    ModelState.AddModelError("mail", "Veuillez écrire un email valide");
+                }
+            }
+            else
+            {
+                //Message d'erreur si le mail est vide ou null
+                ModelState.AddModelError("mail", "Veuillez remplir ce champ");
+            }
+            if (!String.IsNullOrEmpty(brokerList.phoneNumber)) //si le champ phoneNumber n'est pas vide ou null on vérifie la validité de l'entrée
+            {
+                if (!Regex.IsMatch(brokerList.phoneNumber, regexPhone))
+                {
+                    //Message d'erreur
+                    ModelState.AddModelError("phoneNumber", "Veuillez saisir un numéro valide");
+                }
+            }
+            else
+            {
+                //Message d'erreur si le numéro de téléphone est vide ou null
+                ModelState.AddModelError("phoneNumber", "Veuillez remplir ce champ");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(brokerList).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("successAddBroker");
+            }
+            else
+            {
+                return View("ProfilBroker");
+            }
+        }
+        //GET: ListBroker
         public ActionResult ListBroker()
         {
             return View(db.brokers.ToList());
@@ -31,7 +113,7 @@ namespace MVC2.Controllers
         }
         // Instanciation du constructeur
         [HttpPost]
-        public ActionResult AddBroker([Bind(Include ="idBroker, lastName, firstName, mail, phoneNumber")] broker BrokerToAdd)
+        public ActionResult AddBroker([Bind(Include = "idBroker, lastName, firstName, mail, phoneNumber")] broker BrokerToAdd)
         {
             string regexName = @"^[A-Za-zéèëêuùçôàö\-]+$";
             string regexMail = @"[0-9a-zA-Z\.\-]+@[0-9a-zA-Z\.\-]+.[a-zA-Z]{2,4}";
@@ -39,7 +121,7 @@ namespace MVC2.Controllers
             if (!string.IsNullOrEmpty(BrokerToAdd.lastName))
             {
                 // verification de regex
-                if(!Regex.IsMatch(BrokerToAdd.lastName, regexName))
+                if (!Regex.IsMatch(BrokerToAdd.lastName, regexName))
                 {
                     ModelState.AddModelError("LastName", "Ecrire un nom valide");
                 }
@@ -51,7 +133,7 @@ namespace MVC2.Controllers
             }
             if (!string.IsNullOrEmpty(BrokerToAdd.firstName))
             {
-                if(!Regex.IsMatch(BrokerToAdd.firstName, regexName))
+                if (!Regex.IsMatch(BrokerToAdd.firstName, regexName))
                 {
                     ModelState.AddModelError("firstName", "Ecrire un prénom valide");
                 }
@@ -62,7 +144,7 @@ namespace MVC2.Controllers
             }
             if (!string.IsNullOrEmpty(BrokerToAdd.mail))
             {
-                if(!Regex.IsMatch(BrokerToAdd.mail, regexMail))
+                if (!Regex.IsMatch(BrokerToAdd.mail, regexMail))
                 {
                     ModelState.AddModelError("mail", "Ecrire une adresse mail valide");
                 }
@@ -73,7 +155,7 @@ namespace MVC2.Controllers
             }
             if (!string.IsNullOrEmpty(BrokerToAdd.phoneNumber))
             {
-                if(!Regex.IsMatch(BrokerToAdd.phoneNumber, regexPhone))
+                if (!Regex.IsMatch(BrokerToAdd.phoneNumber, regexPhone))
                 {
                     ModelState.AddModelError("phoneNumber", "Ecrire une numero de tel valide");
                 }
@@ -82,7 +164,7 @@ namespace MVC2.Controllers
             {
                 ModelState.AddModelError("phoneNumber", "Ecrire une numero de tel");
             }
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 db.brokers.Add(BrokerToAdd); // insertion dans la dataBase
                 db.SaveChanges(); // enregistre les changements
---
+++ MVC2/Controllers/CustomerController.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Text.RegularExpressions; // Pour l'utilisation des Regex
+using MVC2.Models;
+using System.Data.Entity;
+using System.Net;
+
+namespace MVC2.Controllers
+{   
+    public class CustomerController : Controller
+    {
+        private NoteBookEntities1 db = new NoteBookEntities1();
+
+        //GET: ListCustomer
+        public ActionResult ListCustomer()
+        {
+            return View(db.customers.ToList());
+        }
+        // GET: Customer
+        public ActionResult AddCustomer()
+        {
+            return View("AddCustomer");
+        }
+        // DELETE: Customer
+        public ActionResult DeleteCustomer()
+        {
+            return View("DeleteCustomer");
+        }
+        //redirection vers la page de succès
+        public ActionResult SuccessAddCustomer()
+        {
+            return View("SuccessAddCustomer");
+        }
+        // Instanciation du constructeur
+        [HttpPost]
+        public ActionResult AddCustomer([Bind(Include = "idCustomer, lastName, firstName, mail, phoneNumber, budget, subject")] customer CustomerToAdd)
+        {
+            string regexName = @"^[A-Za-zéèëêuùçôàö\-]+$";
+            string regexMail = @"[0-9a-zA-Z\.\-]+@[0-9a-zA-Z\.\-]+.[a-zA-Z]{2,4}";
+            string regexPhone = @"^[0][0-9]{9}";
+            string regexSubject = @"^[A-Za-zéèëêuùçôàö\-' ]+$";
+            if (!string.IsNullOrEmpty(CustomerToAdd.lastName))
+            {
+                // verification de regex
+                if (!Regex.IsMatch(CustomerToAdd.lastName, regexName))
+                {
+                    ModelState.AddModelError("LastName", "Ecrire un nom valide");
+                }
+            }
+            // Verification si le champs est libre ou pas
+            else
+            {
+                ModelState.AddModelError("LastName", "Ecrire un nom");
+            }
+            if (!string.IsNullOrEmpty(CustomerToAdd.firstName))
+            {
+                if (!Regex.IsMatch(CustomerToAdd.firstName, regexName))
+                {
+                    ModelState.AddModelError("firstName", "Ecrire un prénom valide");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("firstName", "Ecrire un prénom");
+            }
+            if (!string.IsNullOrEmpty(CustomerToAdd.mail))
+            {
+                if (!Regex.IsMatch(CustomerToAdd.mail, regexMail))
+                {
+                    ModelState.AddModelError("mail", "Ecrire une adresse mail valide");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("mail", "Ecrire une adresse mail");
+            }
+            if (!string.IsNullOrEmpty(CustomerToAdd.phoneNumber))
+            {
+                if (!Regex.IsMatch(CustomerToAdd.phoneNumber, regexPhone))
+                {
+                    ModelState.AddModelError("phoneNumber", "Ecrire une numero de tel valide");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("phoneNumber", "Ecrire une numero de tel");
+            }
+            if (!string.IsNullOrEmpty(CustomerToAdd.subject))
+            {
+                if (!Regex.IsMatch(CustomerToAdd.subject, regexSubject))
+                {
+                    ModelState.AddModelError("subject", "Erreur dans l'écriture du sujet réécrivez s'il vous plait");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("subject", "Ecrire un sujet");
+            }
+            if (ModelState.IsValid)
+            {
+                db.customers.Add(CustomerToAdd); // insertion dans la dataBase
+                db.SaveChanges(); // enregistre les changements
+                return RedirectToAction("SuccessAddCustomer");
+            }
+            else
+            {
+                return View(CustomerToAdd); // Il y'a des erreurs
+            }
+        }
+        // GET: ProfilCustomer
+        public ActionResult ProfilCustomer(int? id)
+        {
+            if (id == null)
+            {
+                return View("ErrorBadRequest");
+            }
+            // recupère le broker par son id. 
+            customer customerById = db.customers.Find(id);
+            if (customerById == null)
+            {
+                return View("ErrorPageNotFound");
+            }
+            return View(customerById);
+        }
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult ProfilCustomer([Bind(Include = "idCustomer,lastName,firstName,mail,phoneNumber,budget,subject")] customer CustomerList)
+        {
+            string regexName = @"^[A-Za-zéèëêuùçôàö\-]+$";
+            string regexMail = @"[0-9a-zA-Z\.\-]+@[0-9a-zA-Z\.\-]+.[a-zA-Z]{2,4}";
+            string regexPhone = @"^[0][0-9]{9}";
+            string regexSubject = @"^[A-Za-zéèëêuùçôàö\-' ]+$";
+            if (!string.IsNullOrEmpty(CustomerList.lastName))
+            {
+                // verification de regex
+                if (!Regex.IsMatch(CustomerList.lastName, regexName))
+                {
+                    ModelState.AddModelError("LastName", "Ecrire un nom valide");
+                }
+            }
+            // Verification si le champs est libre ou pas
+            else
+            {
+                ModelState.AddModelError("LastName", "Ecrire un nom");
+            }
+            if (!string.IsNullOrEmpty(CustomerList.firstName))
+            {
+                if (!Regex.IsMatch(CustomerList.firstName, regexName))
+                {
+                    ModelState.AddModelError("firstName", "Ecrire un prénom valide");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("firstName", "Ecrire un prénom");
+            }
+            if (!string.IsNullOrEmpty(CustomerList.mail))
+            {
+                if (!Regex.IsMatch(CustomerList.mail, regexMail))
+                {
+                    ModelState.AddModelError("mail", "Ecrire une adresse mail valide");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("mail", "Ecrire une adresse mail");
+            }
+            if (!string.IsNullOrEmpty(CustomerList.phoneNumber))
+            {
+                if (!Regex.IsMatch(CustomerList.phoneNumber, regexPhone))
+                {
+                    ModelState.AddModelError("phoneNumber", "Ecrire une numero de tel valide");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("phoneNumber", "Ecrire une numero de tel");
+            }
+            if (!string.IsNullOrEmpty(CustomerList.subject))
+            {
+                if (!Regex.IsMatch(CustomerList.subject, regexSubject))
+                {
+                    ModelState.AddModelError("subject", "Erreur dans l'écriture du sujet réécrivez s'il vous plait");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("subject", "Ecrire un sujet");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(CustomerList).State = EntityState.Modified; // insertion dans la dataBase
+                db.SaveChanges(); // enregistre les changements
+                return RedirectToAction("SuccessAddCustomer");
+            }
+            else
+            {
+                return View(CustomerList); // Il y'a des erreurs
+            }
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customers = db.customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+        // POST: customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            customer customers = db.customers.Find(id);
+            db.customers.Remove(customers);
+            db.SaveChanges();
+            return RedirectToAction("listCustomer");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+}
